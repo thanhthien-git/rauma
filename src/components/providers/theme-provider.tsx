fe0d898fdd48517c81871077e22a57961d61daf6
NGÔ THANH THIÊN <thanhthien.devp@gmail.com>
2025-08-09 17:23:30 +0700

    create page folder

---
+++ src/components/providers/theme-provider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import * as React from "react";
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps,
+} from "next-themes";
+
+export function ThemeProvider({
+  children,
+  ...props
+}: Readonly<ThemeProviderProps>) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
